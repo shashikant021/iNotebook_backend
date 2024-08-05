578fa231890cbcd9aa377767813cbb2fc3d65d60
shashi <shashi@gmaill.com>
2024-08-05 17:29:13 +0530

    backend part done !

--- models/User.js
+++ models/User.js
@@ -3,23 +3,22 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     name:{
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
     email:{
-        type:String,
-        required:true,
+        type: String,
+        required: true,
         unique: true
     },
     password:{
-        type:String,
-        required:true
+        type: String,
+        required: true
     },
     date:{
-        type:Date,
+        type: Date,
         default: Date.now
-    }
+    },
   });
-
   const User = mongoose.model('user', UserSchema);
-  module.exports = User
+  module.exports = User;
