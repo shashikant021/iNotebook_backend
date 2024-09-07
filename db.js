e12ab6f4fbfd49f5aaf41c3bcaf0c95904a1797e
shashikant021 <shashikantsahu487@gmail.com>
2024-09-07 22:22:35 +0530

    latest changes done

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&directConnection=true&ssl=false"
+// mongodb+srv://shashikantsahu487:Sahu%401234@cluster0.fxggr.mongodb.net/iNotebook
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
